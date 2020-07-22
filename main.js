f3d0e31cc3733cf6b928b0697d6d07d50c24232b
Eason <chenzscd@163.com>
2020-07-22 12:52:55 +0800

    [POS Machine] [Eason] refactor

--- main.js
+++ main.js
@@ -5,88 +5,78 @@ function printReceipt(barcodes) {
 
 function mapTheBarcodes(barcodes) {
     let commoditiesInfo = [];
-    for (let i = 0; i < barcodes.length; i++) {
-        let index = null;
-        for (let j = 0; j < commoditiesInfo.length; j++) {
-            if (commoditiesInfo[j].barcode === barcodes[i]) {
-                index = j;
-                break;
-            }
-        }
-        if (index !== null) {
-            let commodity = commoditiesInfo[index]
-            commoditiesInfo[index].quantity ++;
-            commoditiesInfo[index].subtotal = calculateSubtotal(commodity);
+    barcodes.forEach(barcode => {
+        const commodityInfo = commoditiesInfo.find(commodityInfo => {
+            return commodityInfo.barcode === barcode;
+        });
+        if (commodityInfo !== undefined) {
+            commodityInfo.quantity ++;
+            commodityInfo.subtotal = calculateSubtotal(commodityInfo);
         } else {
-            let commodity = getCommodityInfoBybarcode(barcodes[i]);
+            let commodity = getCommodityInfoBybarcode(barcode);
             let commodityInfo = {
-                barcode: barcodes[i],
+                barcode: barcode,
                 name: commodity.name,
                 price: commodity.price,
-                quantity: 1,
-                subtotal: 0
+                quantity: 1
             };
-            commodityInfo.subtotal = calculateSubtotal(commodity, 1);
+            commodityInfo.subtotal = calculateSubtotal(commodityInfo);
             commoditiesInfo.push(commodityInfo);
         }
-    }
+    });
     return commoditiesInfo;
 }
 
-function calculateSubtotal(commodity, quantity) {
-    return quantity ? quantity * commodity.price : commodity.quantity * commodity.price;
+function calculateSubtotal(commodity) {
+    return commodity.quantity * commodity.price;
 }
 
 function getCommodityInfoBybarcode(barcode) {
     const commodities = [
         {
-           barcode: 'ITEM000000',
-           name: 'Coca-Cola',
-           price: 3
-         },
-         {
-           barcode: 'ITEM000001',
-           name: 'Sprite',
-           price: 3
-         },
-         {
-           barcode: 'ITEM000002',
-           name: 'Apple',
-           price: 5
-         },
-         {
-           barcode: 'ITEM000003',
-           name: 'Litchi',
-           price: 15
-         },
-         {
-           barcode: 'ITEM000004',
-           name: 'Battery',
-           price: 2
-         },
-         {
-           barcode: 'ITEM000005',
-           name: 'Instant Noodles',
-           price: 4
-         }
-     ];
-     for (let i = 0; i < commodities.length; i++) {
-         if (commodities[i].barcode === barcode) {
-            return commodities[i];
-         }
-     }
+            barcode: 'ITEM000000',
+            name: 'Coca-Cola',
+            price: 3
+        },
+        {
+            barcode: 'ITEM000001',
+            name: 'Sprite',
+            price: 3
+        },
+        {
+            barcode: 'ITEM000002',
+            name: 'Apple',
+            price: 5
+        },
+        {
+            barcode: 'ITEM000003',
+            name: 'Litchi',
+            price: 15
+        },
+        {
+            barcode: 'ITEM000004',
+            name: 'Battery',
+            price: 2
+        },
+        {
+            barcode: 'ITEM000005',
+            name: 'Instant Noodles',
+            price: 4
+        }
+    ];
+    return commodities.find(commodity => {
+        return commodity.barcode === barcode
+    })
 }
 
 function printTheOutput(commoditiesInfo) {
     let output = '\n***<store earning no money>Receipt ***\n';
     let total = 0;
-    for (let i = 0; i < commoditiesInfo.length; i++) {
-        output += 'Name: ' + commoditiesInfo[i].name + ', Quantity: ' + commoditiesInfo[i].quantity
-         + ', Unit price: ' + commoditiesInfo[i].price + ' (yuan), Subtotal: '
-         + commoditiesInfo[i].subtotal + ' (yuan)\n';
-        total += commoditiesInfo[i].subtotal;
-    }
-    output += '----------------------\nTotal: ' + total + ' (yuan)\n';
+    commoditiesInfo.forEach(commodityInfo => {
+        output += `Name: ${commodityInfo.name}, Quantity: ${commodityInfo.quantity}, Unit price: ${commodityInfo.price} (yuan), Subtotal: ${commodityInfo.subtotal} (yuan)\n`;
+        total += commodityInfo.subtotal;
+    });
+    output += `----------------------\nTotal: ${total} (yuan)\n`;
     output += '**********************';
     console.log(output);
 }
